d586e4e399d4093cba4ea2295d0deb8a89494a77
Alice Jiang <97613431+Alice-2096@users.noreply.github.com>
2023-03-15 20:46:35 -0400

    Update syscall.c

--- project/syscall.c
+++ project/syscall.c
@@ -120,36 +120,47 @@ static int (*syscalls[])(void) = {
     [SYS_trace] sys_trace,
 };
 
-static int traceMode = 0; 
+static char *syscalls_names[] = {
+    [SYS_fork] "fork",
+    [SYS_exit] "exit",
+    [SYS_wait] "wait",
+    [SYS_pipe] "pipe",
+    [SYS_read] "read",
+    [SYS_kill] "kill",
+    [SYS_exec] "exec",
+    [SYS_fstat] "fstat",
+    [SYS_chdir] "chdir",
+    [SYS_dup] "dup",
+    [SYS_getpid] "getpid",
+    [SYS_sbrk] "sbrk",
+    [SYS_sleep] "sleep",
+    [SYS_uptime] "uptime",
+    [SYS_open] "open",
+    [SYS_write] "write",
+    [SYS_mknod] "mknod",
+    [SYS_unlink] "unlink",
+    [SYS_link] "link",
+    [SYS_mkdir] "mkdir",
+    [SYS_close] "close",
+    [SYS_trace] "trace",
+};
 
 void syscall(void)
 {
-  int num;
-  num = proc->tf->eax;
+  int num = proc->tf->eax;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num])
   {
-    proc->tf->eax = syscalls[num]();
-    if (proc->trace == -1)
-    { // turn off trade mode
-      traceMode = 0;
-      return;
-    }
-    else if (proc->trace == 1)
-    { // turn on trace mode
-      traceMode = 1;
-      return;
-    }
-
-    if (traceMode)
+    if (num == SYS_exit && proc->trace)
     {
-      cprintf("TRACE: pid = %d | command_name = %s | syscall_num = %d | return value = %d\n", proc->pid, proc->name, num, proc->tf->eax);
-      //not sure how to print SYSCALL name
+      cprintf("TRACE: pid = %d | command name = %s | syscall = %s \n", proc->pid, proc->name, syscalls_names[num]);
     }
+    proc->tf->eax = syscalls[num]();
+    if (proc->trace)
+      cprintf("TRACE: pid = %d | command name = %s | syscall = %s | return value = %d\n", proc->pid, proc->name, syscalls_names[num], proc->tf->eax);
   }
   else
   {
-    cprintf("%d %s: unknown sys call %d\n",
-            proc->pid, proc->name, num);
+    cprintf("%d %s: unknown sys call %d\n", proc->pid, proc->name, num);
     proc->tf->eax = -1;
   }
 }
