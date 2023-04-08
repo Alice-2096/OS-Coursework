025719aae40dcb61e1870b0176376143c5c062f1
Alice Jiang <97613431+Alice-2096@users.noreply.github.com>
2023-04-08 00:12:59 -0400

    Update syscall.c

--- project/syscall.c
+++ project/syscall.c
@@ -94,6 +94,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_trace(void);
+extern int sys_dump(void);
 
 static int (*syscalls[])(void) = {
     [SYS_fork] sys_fork,
@@ -118,6 +119,7 @@ static int (*syscalls[])(void) = {
     [SYS_mkdir] sys_mkdir,
     [SYS_close] sys_close,
     [SYS_trace] sys_trace,
+    [SYS_dump] sys_dump,
 };
 
 static char *syscalls_names[] = {
@@ -143,20 +145,87 @@ static char *syscalls_names[] = {
     [SYS_mkdir] "mkdir",
     [SYS_close] "close",
     [SYS_trace] "trace",
+    [SYS_dump] "dump",
 };
+
+static char *events_cmd_name[DUMPSIZE];
+static char *events_syscall[DUMPSIZE];
+static int events_return_value[DUMPSIZE];
+static int events_pid[DUMPSIZE];
+static int full = 0;
+static int curr = 0;
+
+// TODO take in the index where exit syscall of the current program is stored --> curr - 1
+// TODO backtracking all the way to the first syscall.
+// TODO then prints from first to exit
+void printEvents(int pid)
+{
+  int start = full ? curr : 0;
+  int i, j = 0;
+  for (i = start; j < DUMPSIZE; j++)
+  {
+    cprintf("DUMP: pid = %d | command name = %s | syscall = %s | return value = %d\n ", events_pid[i], events_cmd_name[i], events_syscall[i], events_return_value[i]);
+
+    if (i < DUMPSIZE - 1)
+      i++;
+    else
+      i = 0;
+  }
+}
 
 void syscall(void)
 {
   int num = proc->tf->eax;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num])
   {
-    if (num == SYS_exit && proc->trace)
+    if (num == SYS_dump)
     {
-      cprintf("TRACE: pid = %d | command name = %s | syscall = %s \n", proc->pid, proc->name, syscalls_names[num]);
+      printEvents(-1);
+      return;
+    }
+
+    //  update dump
+    //  cprintf("curr ptr is: %d | curr event is: %s\n", curr, syscalls_names[num]);
+    events_syscall[curr] = syscalls_names[num]; //! memset here?
+    events_cmd_name[curr] = proc->name;
+    events_pid[curr] = proc->pid;
+    curr++;
+    if (curr == DUMPSIZE)
+    {
+      curr = 0;
+      if (!full)
+        full = 1;
+    }
+
+    //  trace
+    if (num == SYS_exit && proc->trace && !proc->fflag)
+    {
+      if (!proc->eflag || !strncmp(proc->syscallName, "exit", strlen("exit")))
+      {
+        cprintf("TRACE: pid = %d | command name = %s | syscall = %s \n", proc->pid, proc->name, syscalls_names[num]);
+      }
     }
     proc->tf->eax = syscalls[num]();
-    if (proc->trace)
+    // cprintf("process eflag:%d eflag name:%s | fflag %d sfflag %d \n", proc->eflag, proc->syscallName, proc->fflag, proc->sflag);
+    events_return_value[curr] = proc->tf->eax;
+
+    if (proc->sflag && proc->tf->eax == -1)
+    {
+      // print nothing
+    }
+    else if (proc->fflag && proc->tf->eax == 0)
+    {
+      // print nothing
+    }
+    else if (proc->eflag && strncmp(proc->syscallName, syscalls_names[num], strlen(proc->syscallName)))
+    {
+      // print nothing if syscall name does not match
+    }
+    else if (proc->trace)
+    {
       cprintf("TRACE: pid = %d | command name = %s | syscall = %s | return value = %d\n", proc->pid, proc->name, syscalls_names[num], proc->tf->eax);
+      // cprintf("process eflag:%d eflag name:%s | fflag %d sfflag %d \n", proc->eflag, proc->syscallName, proc->fflag, proc->sflag);
+    }
   }
   else
   {
